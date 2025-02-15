b85e8a0e3da2e69197aa694b153189a642aad76d
Endale2 <endale406@gmail.com>
2025-02-15 16:48:03 +0300

    updated

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,20 +1,32 @@
+import 'package:coffee_app/components/coffee_tile.dart';
+import 'package:coffee_app/entities/coffee.dart';
+import 'package:coffee_app/entities/coffee_shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopePage extends StatelessWidget {
   const ShopePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: Column(
-        children: [
-          Text("How Would You Like Your Coffee?",
-              style: TextStyle(fontSize: 20)),
-          const SizedBox(height: 25)
-        ],
-      ),
-    ));
+    return Consumer<CoffeeShop>(
+        builder: (context, value, child) => SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: [
+                  Text("How Would You Like Your Coffee?",
+                      style: TextStyle(fontSize: 20)),
+                  const SizedBox(height: 25),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: value.coffeeShop.length,
+                          itemBuilder: (context, index) {
+                            Coffee coffee = value.coffeeShop[index];
+                            return CoffeeTile(coffee: coffee);
+                          }))
+                ],
+              ),
+            )));
   }
 }
